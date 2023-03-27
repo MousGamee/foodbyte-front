c99a7e9fdb1f682dba593f5a0d96aeb96ca3040e
Fatou Fofana <fatoufofana@MacBook-Air-de-Fatou.local>
2023-03-27 23:03:54 +0200

    end loginScreen

--- src/components/CustumTextInput.jsx
+++ src/components/CustumTextInput.jsx
@@ -3,17 +3,40 @@ import React from 'react'
 import { COLORS, SIZES } from '../constants'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
-const CustumTextInput = () => {
-  return (
-    <View style={{
-        height : 70,
-        borderRadius : 20,
-        backgroundColor : COLORS.lightGrey20
-    }}>
-        <Icon name='email' color={COLORS.white} size={60}/>
-      <TextInput/>
-    </View>
-  )
+const CustumTextInput = ({
+    placeholder,
+    keyboardType,
+    iconName,
+    secureTextEntry,
+    moreStyle
+}) => {
+    return (
+        <View style={{
+            ...moreStyle,
+            height: SIZES.width * .16,
+            borderRadius: 15,
+            backgroundColor: COLORS.lightGrey20,
+            flexDirection: 'row',
+            alignItems: 'center'
+        }}>
+            <TextInput
+            secureTextEntry={secureTextEntry}
+            placeholder={placeholder}
+            placeholderTextColor={COLORS.light60}
+            keyboardType={keyboardType}
+                style={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    paddingLeft: SIZES.padding * 2.7,
+                    color : COLORS.white,
+                }}
+            />
+            <Icon name={iconName} color={COLORS.white} size={SIZES.width * .08} style={{
+                marginLeft: SIZES.margin,
+            }} />
+        </View>
+    )
 }
 
 export default CustumTextInput
